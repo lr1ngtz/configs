b803aed71858dba883ee9f3c6336825aa74254e7
lr1ngtz <sko0hlol@gmail.com>
2025-07-18 20:48:59 +0200

    Add .emacs.d dir

---
+++ .emacs.d/elpa/pbcopy-20150225.459/pbcopy-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "pbcopy" "20150225.459"
+  "Emacs Interface to pbcopy."
+  ()
+  :url "https://github.com/jkp/pbcopy.el"
+  :commit "338f7245746b5de1bb96c5cc2b32bfd9b5d83272"
+  :revdesc "338f7245746b"
+  :keywords '("mac" "osx" "pbcopy"))
