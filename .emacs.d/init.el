2e6c1d2dbbbac05649a1be45413789445b024c53
lr1ngtz <sko0hlol@gmail.com>
2025-10-07 22:16:20 +0200

    update emacs, and zshrc configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,30 @@
-(setq custom-file "~/.emacs.d/emacs.custom.el")
-(load custom-file)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruvbox-dark-medium))
+ '(custom-safe-themes
+   '("09276f492e8e604d9a0821ef82f27ce58b831f90f49f986b4d93a006c12dbcdb"
+     "75b371fce3c9e6b1482ba10c883e2fb813f2cc1c88be0b8a1099773eb78a7176"
+     "18a1d83b4e16993189749494d75e6adb0e15452c80c431aca4a867bcc8890ca9"
+     "51fa6edfd6c8a4defc2681e4c438caf24908854c12ea12a1fbfd4d055a9647a3"
+     "8363207a952efb78e917230f5a4d3326b2916c63237c1f61d7e5fe07def8d378"
+     "5a0ddbd75929d24f5ef34944d78789c6c3421aa943c15218bac791c199fc897d"
+     "276228257774fa4811da55346b1e34130edb068898565ca07c2d83cfb67eb70a"
+     default))
+ '(package-selected-packages
+   '(consult go-mode gruvbox-theme marginalia orderless pbcopy vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq scroll-margin 5)
 ;; indenting
 (setq-default indent-tabs-mode nil)
@@ -13,26 +37,20 @@
 (set-face-background 'whitespace-trailing "#cc0000") ; red background
 (set-face-foreground 'whitespace-trailing nil)
 
-;; Set default font for emacs gui
-(add-to-list 'default-frame-alist '(font . "JetBrains Mono-14"))
-
 ;; enable c-x c-l to downcase selected region
 (put 'downcase-region 'disabled nil)
 ;; enable c-x c-u to upcase selected region
 (put 'upcase-region 'disabled nil)
+
 ;; don't show the splash screen
 (setq inhibit-startup-message t)
 (menu-bar-mode -1)
-(tool-bar-mode -1)
+
 ;; makes sure the contents of the buffer is refreshed automatically when the file is changed outside of emacs
 (global-auto-revert-mode t)
 ;; select scope
 (global-set-key (kbd "C-c s") 'mark-sexp)
 (global-set-key (kbd "C-x ;") 'comment-line)
-
-;; ensure emacs can find executables installed by homebrew on intel macos
-;; (add-to-list 'exec-path "/usr/local/bin")
-;; (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 
 ;; melpa
 (require 'package)
@@ -58,12 +76,6 @@
   :mode "\\.odin\\'"
   :defer t)
 
-;; golang
-(use-package go-mode
-  :ensure t
-  :mode "\\.go\\'"
-  :defer t)
-
 ;; display line numbers in every buffer
 ;; (global-display-line-numbers-mode)
 
@@ -84,45 +96,11 @@
         (concat "~/.emacs.d/backup/"
                 (file-name-nondirectory filename))))
 
-;; ;; enable pbcopy (macos)
-;; (use-package pbcopy
-;;   :init
-;;   (turn-on-pbcopy)  ; integrate with system clipboard
-;;   (setq select-enable-clipboard t)
-;;   :defer t)
-
-
-;;; enable vertico, consult, orderless, and marginalia
-(use-package vertico
+;; enable pbcopy (macos)
+(use-package pbcopy
   :init
-  (vertico-mode 1)
-  :defer t)
-
-(use-package consult
-  :bind (("M-g g" . consult-goto-line)
-         ("C-c f" . consult-fd)
-         ("C-c r" . consult-ripgrep)
-         ("C-c o" . odin-source-search))
-  :config
-  (defun odin-source-search (search-term) ;;; odin source code default ripgrep case-insensetive
-    "Search for SEARCH-TERM in core, vendor, and base under ~/Odin using consult-ripgrep."
-    (interactive "sSearch term: ")
-    (let ((base-path (expand-file-name "~/Odin")))
-      (unless (file-directory-p base-path)
-        (error "Odin directory %s does not exist" base-path))
-      (consult-ripgrep base-path (concat search-term " -- core vendor base -i"))))
-  :defer t)
-
-(use-package orderless
-  :init
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion))))
-  :defer t)
-
-(use-package marginalia
-  :init
-  (marginalia-mode 1)
+  (turn-on-pbcopy)  ; integrate with system clipboard
+  (setq select-enable-clipboard t)
   :defer t)
 
 
@@ -135,56 +113,41 @@
 ;;; compile
 (global-set-key (kbd "C-c c") 'compile)
 
+;;; consult group ################################################################
+(use-package vertico
+  :init
+  (vertico-mode 1))
 
-;; requires build directory and project.el file in the root of the project
-(defun run-odin-compile ()
-  "Run 'odin run .'"
-  (interactive)
-  (let ((default-directory (if (project-current)
-                               (project-root (project-current))
-                             default-directory)))
-    (compile "odin run . -out:build/main")))
-(global-set-key (kbd "C-c l") 'run-odin-compile)
+(use-package consult
+  :bind (("M-g g" . consult-goto-line)
+         ("C-c f" . consult-find)
+         ;; ("C-c r" . consult-ripgrep)
+         ("C-c r" . consult-grep)))
 
-(defun run-odin-debug-compile ()
-  "Run 'odin run . -debug'"
-  (interactive)
-  (let ((default-directory (if (project-current)
-                               (project-root (project-current))
-                             default-directory)))
-    (compile "odin run . -debug -out:build/main")))
-(global-set-key (kbd "C-c C-l") 'run-odin-debug-compile)
+(use-package orderless
+  :init
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
 
-(defun build-odin-compile ()
-  "Run 'odin build .'"
-  (interactive)
-  (let ((default-directory (if (project-current)
-                               (project-root (project-current))
-                             default-directory)))
-    (compile "odin build . -out:build/main")))
-(global-set-key (kbd "C-c b") 'build-odin-compile)
-
-(defun build-odin-debug-compile ()
-  "Run 'odin build . -debug'"
-  (interactive)
-  (let ((default-directory (if (project-current)
-                               (project-root (project-current))
-                             default-directory)))
-    (compile "odin build . -debug -out:build/main")))
-(global-set-key (kbd "C-c C-b") 'build-odin-debug-compile)
+(use-package marginalia
+  :init
+  (marginalia-mode 1))
+;;; consult group ################################################################
 
 
-(defun my/switch-theme-by-time ()
-  "switch emacs theme based on the current time."
-  (let ((hour (string-to-number (format-time-string "%H"))))
-    (if (or (>= hour 19) (< hour 7))
-        ;; nighttime (7 pm to 7 am): load dark theme
-        (load-theme 'gruvbox-dark-medium t)
-      ;; daytime (7 am to 7 pm): load light theme
-      (load-theme 'gruvbox-light-soft t))))
+;; (defun my/switch-theme-by-time ()
+;;   "switch emacs theme based on the current time."
+;;   (let ((hour (string-to-number (format-time-string "%H"))))
+;;     (if (or (>= hour 19) (< hour 7))
+;;         ;; nighttime (7 pm to 7 am): load dark theme
+;;         (load-theme 'gruvbox-dark-medium t)
+;;       ;; daytime (7 am to 7 pm): load light theme
+;;       (load-theme 'gruvbox-light-soft t))))
 
-;; run the theme switcher when emacs starts
-(add-hook 'emacs-startup-hook 'my/switch-theme-by-time)
+;; ;; run the theme switcher when emacs starts
+;; (add-hook 'emacs-startup-hook 'my/switch-theme-by-time)
 
-;; Periodically check the time and switch theme if needed
-(run-at-time "0 sec" 7200 'my/switch-theme-by-time)
+;; ;; Periodically check the time and switch theme if needed
+;; (run-at-time "0 sec" 7200 'my/switch-theme-by-time)
+
--- .zshrc
+++ .zshrc
@@ -1,22 +1,14 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password mpts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
 # If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:/usr/local/bin:$PATH
+# export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 
-# Path to your oh-my-zsh installation.
+# Path to your Oh My Zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
-# load a random theme each time oh-my-zsh is loaded, in which case,
+# load a random theme each time Oh My Zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-# ZSH_THEME="robbyrussell"
-ZSH_THEME="powerlevel10k/powerlevel10k"
+ZSH_THEME="robbyrussell"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -79,9 +71,11 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(
-	git
-	zsh-autosuggestions
-    # zsh-syntax-highlighting
+    git
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    # fast-syntax-highlighting
+    # zsh-autocomplete
 )
 
 source $ZSH/oh-my-zsh.sh
@@ -97,66 +91,50 @@ source $ZSH/oh-my-zsh.sh
 # if [[ -n $SSH_CONNECTION ]]; then
 #   export EDITOR='vim'
 # else
-#   export EDITOR='mvim'
+#   export EDITOR='nvim'
 # fi
 
 # Compilation flags
-# export ARCHFLAGS="-arch x86_64"
+# export ARCHFLAGS="-arch $(uname -m)"
 
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# Set personal aliases, overriding those provided by Oh My Zsh libs,
+# plugins, and themes. Aliases can be placed here, though Oh My Zsh
+# users are encouraged to define aliases within a top-level file in
+# the $ZSH_CUSTOM folder, with .zsh extension. Examples:
+# - $ZSH_CUSTOM/aliases.zsh
+# - $ZSH_CUSTOM/macos.zsh
 # For a full list of active aliases, run `alias`.
 #
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
-#virtualenvwrapper
-export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-export VIRTUALENVWRAPPER_VIRTUALENV=/Users/pavelklachkou/Library/Python/3.9/bin/virtualenv
-export PROJECT_HOME=$HOME/Devel
-source /Users/pavelklachkou/Library/Python/3.9/bin/virtualenvwrapper.sh
 
-export PATH="/usr/local/opt/libpq/bin:$PATH"
-alias python=/usr/bin/python3
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-source ~/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme
-export PATH="/usr/local/opt/libpq/bin:$PATH"
 
-export HOMEBREW_NO_AUTO_UPDATE=1
+
+###########################################################################################################
+# pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+eval "$(pyenv init - zsh)"
 
-# Start ssh-agent and add the SSH keys
-if [ -z "$SSH_AUTH_SOCK" ]; then
-    eval "$(ssh-agent -s)"
-    ssh-add --apple-use-keychain ~/.ssh/bitbucket.pem
-fi
-
-# golang
-# Go environment variables
-export GOPATH=$HOME/go
-export GOROOT=/usr/local/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-
-# Odin
+# Odin lang
 export PATH="$HOME/Odin:$PATH"
 export ODIN_ROOT="$HOME/Odin"
 
-. "$HOME/.local/bin/env"
 
-
+# emacs
 alias ec="emacsclient -c"
+alias et="emacsclient -t"
 export EDITOR="emacsclient -t"
 export CODEEDITOR="emacsclient -c -a 'emacs'"
 export VISUAL="emacsclient -c -a 'emacs'"
 export SUDO_EDITOR="emacsclient -t"
 
-export TERM="xterm-256color"
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
 
-source $HOME/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# postgres
+export PATH="/opt/homebrew/opt/postgresql@16/bin:$PATH"
+
